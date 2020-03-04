0f840fc1468894f678965784f11c2455a61cb420
ViktorKollin <61697032+ViktorKollin@users.noreply.github.com>
2020-03-04 11:32:26 +0100

    Uppdated contoller and client

--- src/Client/ClientController.java
+++ src/Client/ClientController.java
@@ -1,4 +1,20 @@
 package Client;
 
 public class ClientController {
+    private Client client;
+
+
+    public ClientController(Client client){
+        this.client = client;
+        client.setClientController(this);
+    }
+
+
+    public void send(Message message){
+        client.send(message);
+    }
+    public void disconnect(){
+        client.disconnect();
+    }
+
 }
