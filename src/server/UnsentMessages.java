7ecb8c5656c243ece6a3589f686402135b7fa7fe
P4CIFIC <malek_123_chawki@hotmail.com>
2020-03-04 16:32:12 +0100

    Added UnsentMessages

--- src/server/UnsentMessages.java
+++ src/server/UnsentMessages.java
@@ -9,5 +9,9 @@ import java.util.HashMap;
 public class UnsentMessages {
     private HashMap<User, ArrayList<Message>> unsent = new HashMap<>();
 
-    public synchronized void put()
+    public synchronized void put(User user, Message message){
+
+        unsent.put(u)
+
+    }
 }
