5b6007e8a894b94574fc6640687f9a3b2148ff23
darweshammar <61697014+darweshammar@users.noreply.github.com>
2020-03-04 10:55:19 +0100

    viktor done

---
+++ src/ServerController.java
@@ -0,0 +1,94 @@
+import com.sun.security.ntlm.Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerController {
+
+    private int port;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    private ServerSocket serverSocket;
+    private ArrayList<ClientHandler> listClientHandler = new ArrayList<>();
+
+    public ServerController(int port) {
+
+        new Connection(port).start();
+
+    }
+
+    private class Connection extends Thread {
+
+        private int port;
+
+        public Connection(int port) {
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            Socket socket;
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+                while (true) {
+                    try {
+                        socket = serverSocket.accept();
+                        listClientHandler.add(new ClientHandler(socket));
+                        System.out.println();
+                    } catch (IOException e) {
+                    }
+                }
+            } catch (IOException e) {
+
+            }
+
+        }
+    }
+
+    private class ClientHandler extends Thread{
+
+        private Socket socket;
+        private ObjectInputStream ois;
+        private ObjectOutputStream dos;
+
+
+        public ClientHandler(Socket socket) throws IOException {
+
+            this.socket = socket;
+            ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+            dos = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            start();
+        }
+
+        @Override
+        public void run() {
+            while (true){
+
+                try {
+                    Object obj = ois.readObject();
+                    if(//personen finns i lista dvs inloggad
+                    //om inte så kolla kontaktlistan sen skicka
+                    // ) {
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+
+            } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+    }
+
+
+}
